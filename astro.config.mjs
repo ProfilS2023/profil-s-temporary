0d824d3dfdd403246a4a3049f5dd428077264310
Александр Орешкин <oreshkin.dev@outlook.com>
2023-12-12 05:05:08 +0300

    Поправил конфиг для pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://profils2023.github.io',
+    base: '/profil-s-temporary',
+});
